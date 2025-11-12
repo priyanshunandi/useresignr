2d929b742af5c801cf2e2fe270c5d6dc39667d32
priyanshunandi <mr.p.n.nandi@gmail.com>
2025-11-12 15:03:39 +0530

    Add files via upload

---
+++ lib/types.ts
@@ -0,0 +1,16 @@
+export interface JobAnalysisData {
+  jobTitle: string
+  yearsOfExperience: number
+  currentSalary: number
+  industrySalary: number
+  qualification: string
+  skillsMatch: string
+  betterSkillsOtherRole: string
+  incomeDependency: string
+  growthOpportunities: number
+  alignWithQualifications: string
+  workLifeBalance: number
+  mentalHealthImpact: number
+  learningOpportunities: number
+  promotionOutlook: string
+}
